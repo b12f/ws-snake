bbb0de18ac7eb7c6bc59cbbab5475be9e9fa6516
Valentin Brüggemann <brueggemann@convidera.com>
2018-09-02 17:42:40 +0200

    socket finish

--- server/index.js
+++ server/index.js
@@ -1,13 +1,31 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+const state = {
+    players: [],
+    action: [],
+    
+};
+
+io.on('connection', function(socket){
+    socket.on('playerJoined', function (msg) {
+        state.players[socket.id] = msg;
+        console.log(state.players);
+    });
+
+    socket.on('playerAction', function (msg) {
+        state.players.push(msg);
+        console.log(state.players);
+    });
+});
+
+setInterval(function () {
+    io.emit('tick', state);
+}, 500);
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', function(socket){
-    console.log('a user connected');
 });
 
 http.listen(3000, function(){
